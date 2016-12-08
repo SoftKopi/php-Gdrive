c1225d983feabf7f98156b48ac55c78aeb71a92f
BartWeyder <jackjunior98@gmail.com>
2016-12-08 23:13:42 +0200

    folder creation now works

--- google_folders.php
+++ google_folders.php
@@ -8,22 +8,33 @@ function search_directory ($folder_name, $folder_parent_id, $driveService) {
     $pageToken = null;
     do {
         $response = $driveService->files->listFiles(array(
-            'q' => "title='$folder_name' and '$folder_parent_id' in parents",
+            'q' => "name='$folder_name' and '$folder_parent_id' in parents",
             'spaces' => 'drive',
             'pageToken' => $pageToken,
-            'fields' => 'nextPageToken, id',
+            'fields' => 'nextPageToken, files(id)',
         ));
     } while ($pageToken != null);
 
-    if ($response->id=="") {
+    $search_folderId = "";
+
+    $search_folderId = $response->files[0]->id;
+    echo $search_folderId;
+
+    /*foreach ($response->files as $file_info) {
+        printf("Found file: (%s)\n", $file_info->id);
+        $search_folderId = $file_info->id;
+    }*/
+
+    if ($search_folderId=="") {
         $fileMetadata = new Google_Service_Drive_DriveFile(array(
         'name' => $folder_name,
         'mimeType' => 'application/vnd.google-apps.folder',
         'parents' => array($folder_parent_id)));
         $file = $driveService->files->create($fileMetadata, array(
             'fields' => 'id'));
+        $search_folderId=$file->id;
     }
-    return $response->id;
+    return $search_folderId;
 }
 
 //get folder Id for upload;
